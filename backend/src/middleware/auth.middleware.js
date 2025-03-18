950db6eff46f8a5af83eb41a67473e7a30d6c7a9
Ritika Sah <ritikasah731@gmail.com>
2025-03-18 21:52:41 +0530

    Changes done

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -1,5 +1,5 @@
 import jwt from "jsonwebtoken";
-import User from "../models/user.model";
+import User from "../models/user.model.js";
 
 export const protectRoute = async(req,res,next) => {
     try {
@@ -8,7 +8,17 @@ export const protectRoute = async(req,res,next) => {
             return res.status(401).json({message: "Unauthorized - No token Provided"})
         }
         const decoded = jwt.verify(token,process.env.JWT_SECRET)
+        if(!decoded){
+            return res.status(401).json({message: "Unauthorized - Invalid Token"})
+        }
+        const user = await User.findById(decoded.userId).select("-password")
+        if(!user){
+            return res.status(401).json({message: "User Not found"})
+        }
+        req.user = user 
+        next()
     } catch (error) {
-        
+        console.log("Error in protectRoute middleware:", error.message)
+        return res.status(500).json({message: "Internal Server Error"})
     }
 }
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,5 +1,6 @@
 import express from "express"
-import {login,logout,signup} from "../controllers/auth.controller.js"
+import {login,logout,signup, updateProfile} from "../controllers/auth.controller.js"
+import { protectRoute } from "../middleware/auth.middleware.js"
 
 const router = express.Router()
 router.post("/signup", signup)
@@ -8,6 +9,6 @@ router.post("/login",login)
 
 router.post("/logout",logout)
 
-// router.put("/update-profile", protectRoute,updateProfile)
+router.put("/update-profile", protectRoute,updateProfile)
 
 export default router;
